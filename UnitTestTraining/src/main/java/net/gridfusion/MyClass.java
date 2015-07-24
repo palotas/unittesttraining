a800f95077c546489ddc4cebcaf96eccf2a551ef
Michael PalotasVM <michael.palotas@gridfusion.net>
2015-07-24 09:48:37 +0200

    another new version

--- UnitTestTraining/src/main/java/net/gridfusion/MyClass.java
+++ UnitTestTraining/src/main/java/net/gridfusion/MyClass.java
@@ -1,13 +1,12 @@
 package net.gridfusion;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MyClass {
+  public int multiply(int x, int y) {
+    // the following is just an example
+    if (x > 999) {
+      throw new IllegalArgumentException("X should be less than 1000");
     }
-}
+    return x / y;
+  }
+} 
+
